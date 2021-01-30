7b539698c72687379e3823fad7b4e116717b2321
nehamarne27 <nehamarne27gmail,com>
2021-01-30 19:38:51 +0530

    some changes needed

--- src/rrt/src/rrt_node.py
+++ src/rrt/src/rrt_node.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-
+#pblm1 node being created inside obstacle region
+#pblm2 area more than map i.e. surounding area being explored w.r.t. edge not vertices 
+#pblm3 every node is related to node 1&2
+#!!!!!!!!!!!!x and y have been shuffeled
+#add condition for edge not being in obstacle
 import rospy
 import cv2
 from sensor_msgs.msg import Image
@@ -11,6 +15,8 @@ def randpointgen():
 	#random generator
 	x = random.randint(0,194)
 	y = random.randint(0,231)
+	#if cv_image[x,y]<200:
+	#	randpointgen()
 	return x,y
 
 cv_image = cv2.imread("/home/neha/Downloads/turtlebot/src/maps/playground1.pgm",cv2.IMREAD_UNCHANGED)
@@ -23,7 +29,7 @@ def main(args):
 	cv2.imshow("neha", cv_image)
 	#print(cv_image.shape)
 	#print(cv_image[140])
-	#o/p of shape is 194,231
+	#o/p of shape is x=231 y=194
 	#make binary file 
 	#define start and end pose
 	start = randpointgen()
@@ -44,17 +50,12 @@ def main(args):
 			if np.linalg.norm(nodes[i]-rand_pt)<mini:
 				min_node = nodes[i]
 				mini = np.linalg.norm(nodes[i] - rand_pt)	
-		#print(mini)
-		#print(min_node)
-		#align in direction and fix the distance to get the node
 		new_node = min_node+rand_pt
 		new_node = new_node/2
-		if(np.linalg.norm(new_node - goal)<30):
-			print_path(nodes,graph,start)
+		if(np.linalg.norm(new_node - goal)<10):
 			print("goal reached")
 			break
 		#print(cv_image[new_node[0],new_node[1]])
-		#add condition for edge not being in obstacle
 		if cv_image[new_node[0],new_node[1]]>=210:
 			n = len(nodes[:,0])
 			graph = np.concatenate( (graph,[np.zeros(n)-1]) ,axis=0)
@@ -63,16 +64,16 @@ def main(args):
 			graph[np.where(nodes == min_node),len(nodes[:,0])] = mini/2
 			graph[len(nodes[:,0]),len(nodes[:,0])] = 0
 			nodes = np.concatenate((nodes,[new_node]))
-		#else:
-		#	break
-		print("i got in")
-		#print(nodes)
-		print(graph)
-		cv2.circle(cv_image, (new_node[0],new_node[1]), 1,(0,255,0),-1)
-		cv2.line(cv_image,(min_node[0],min_node[1]),(new_node[0],new_node[1]),(150),1)
-		cv2.imshow("neha", cv_image)
-		print(len(nodes[:,0]))
-		print("m here")
+			print("i got in")
+			print(nodes)
+			print(cv_image[new_node[0],new_node[1]])
+			#print(graph)
+			cv2.circle(cv_image, (new_node[0],new_node[1]), 1,(0),-1)
+			cv2.line(cv_image,(min_node[0],min_node[1]),(new_node[0],new_node[1]),(150),1)
+			cv2.imshow("neha", cv_image)
+			print(len(nodes[:,0]))
+			print("m here")
+			print(goal)
 	cv2.waitKey()
 	print("m out")
     except KeyboardInterrupt:
@@ -85,7 +86,7 @@ def print_path(nodes,graph,start):
 	while (1):
 		arr = np.where(graph[i] > 0)
 		i = arr[0]
-		i = i[0]
+		print(i)
 		new_pt = nodes[i]
 		cv2.line(cv_image,(pt[0],pt[1]),(new_pt[0],new_pt[1]),(150),1)
 		pt = new_pt
