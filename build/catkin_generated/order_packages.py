81a8b4f258b8050403ba72ae98ea81841d5df7bb
Neha Marne <51331480+marneneha@users.noreply.github.com>
2023-01-24 20:22:06 +0100

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/neha/motion_planning/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/neha/visual_odom/devel;/home/neha/workspace/devel;/home/neha/mrs_workspace/devel;/opt/ros/melodic'.split(';') if '/home/neha/visual_odom/devel;/home/neha/workspace/devel;/home/neha/mrs_workspace/devel;/opt/ros/melodic' != '' else []
--- build/rrt/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "rospy;std_msgs;geometry_msgs;nav_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lrrt".split(';') if "-lrrt" != "" else []
-PROJECT_NAME = "rrt"
-PROJECT_SPACE_DIR = "/home/neha/motion_planning/devel"
-PROJECT_VERSION = "0.0.0"
